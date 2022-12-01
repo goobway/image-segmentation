f5b0059b3ae29dde327c2655e97282fd63bae85a
goobway <calistagreenway@gmail.com>
2022-12-01 01:23:50 -0500

    added kmeans code

--- superpixel_segmentation/final_project2_main.py
+++ superpixel_segmentation/final_project2_main.py
@@ -1,6 +1,7 @@
+import numpy as np
+import cv2
 from skimage.future import graph
 from skimage import segmentation, color, filters
-import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
 
@@ -22,10 +23,12 @@ img2 = img2[:, 90:1536]
 
 
 # felzenszwalb’s algorithm (method 1)
-def felzenszwalb(input_image):
+def felz(input_image):
     img = input_image
     segmented_image = segmentation.felzenszwalb(
         img, scale=500, sigma=0.5, min_size=5000)
+
+    # plot images
     fig = plt.figure(figsize=(12, 12))
     a = fig.add_subplot(1, 3, 1)
     plt.imshow(img)  # original image
@@ -54,6 +57,8 @@ def felzenszwalb_rag(input_image):
 
     out = color.label2rgb(labels2, img, kind='avg', bg_label=0)
     out = segmentation.mark_boundaries(out, labels2, mode='thick')
+
+    # plot images
     fig = plt.figure(figsize=(12, 12))
     a = fig.add_subplot(1, 3, 1)
     plt.imshow(labels)  # before merge
@@ -64,7 +69,7 @@ def felzenszwalb_rag(input_image):
     plt.show()
 
     # results
-    print(f'Felzenszwalb number of segments: {len(np.unique(out))}')
+    print(f'Felzenszwalb with RAG number of segments: {len(np.unique(out))}')
 
 
 def _weight_mean_color(graph, src, dst, n):  # function from plot_rag_merge.py
@@ -80,14 +85,63 @@ def merge_mean_color(graph, src, dst):  # function from plot_rag_merge.py
         graph.nodes[dst]['total color'] / graph.nodes[dst]['pixel count'])
 
 
+# slic superpixels (method 3)
+def slic_superpixels(input_image):
+    pass
+
+
+# kmeans clutersing (method 4 and 5)
+def kmeans_clustering(input_image):
+    img = input_image
+    Z = img.reshape((-1, 3))
+    # convert to np.float32
+    Z = np.float32(Z)
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 8
+    ret, label, center = cv2.kmeans(
+        Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    # convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img.shape))
+
+    # results
+    print(f'Kmeans number of segments: {len(np.unique(res2))}')
+    return res2
+
+
+"""
+Note:   the code below will produce 20 different figures
+        for testing purposes, run only one section/method at a time
+"""
+
 # method 1
-# felzenszwalb(img0)
-# felzenszwalb(img1)
-# felzenszwalb(img2)
-# felzenszwalb(img3)
+# felz(img0)
+# felz(img1)
+# felz(img2)
+# felz(img3)
 
 # method 2
-felzenszwalb_rag(img0)
+# felzenszwalb_rag(img0)
 # felzenszwalb_rag(img1)
 # felzenszwalb_rag(img2)
 # felzenszwalb_rag(img3)
+
+# method 3
+# slic_superpixels(img0)
+# slic_superpixels(img1)
+# slic_superpixels(img2)
+# slic_superpixels(img3)
+
+# method 4
+# slic_superpixels(kmeans_clustering(img0))
+# slic_superpixels(kmeans_clustering(img1))
+# slic_superpixels(kmeans_clustering(img2))
+# slic_superpixels(kmeans_clustering(img3))
+
+# method 5
+# felzenszwalb_rag(kmeans_clustering(img0))
+# felzenszwalb_rag(kmeans_clustering(img1))
+# felzenszwalb_rag(kmeans_clustering(img2))
+# felzenszwalb_rag(kmeans_clustering(img3))
